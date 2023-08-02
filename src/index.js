2a5273f7c32cf3872f2000563d609fa390ad6fd4
Natalia Repkina <pn.88@mail.ru>
2023-08-02 11:05:13 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = String(n);
+    if (n < 0) {
+        str = String(n).slice(1);
+    }
+    return +str.split("").reverse().join("");
+};
